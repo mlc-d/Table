0ed103076f2120b6d34e0128e0fd2ae8c25d671c
Mateo León Camacho <mateoleoncamacho@proton.me>
2023-02-12 18:01:30 -0500

    Test if database connection works

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"log"
 
+	"gitlab.com/mlc-d/table/db"
 	"gitlab.com/mlc-d/table/web/api"
 )
 
 func main() {
+	// test if the database opens
+	_ = db.GetDB()
 	server := api.GetServer()
 	log.Fatalln(server.Start())
 }
